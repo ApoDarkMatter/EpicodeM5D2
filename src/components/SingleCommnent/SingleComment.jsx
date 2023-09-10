a5a77f8ee05ea631d51f114e06d60faa25840c5d
ApoDarkMatter <ndrpln@gmail.com>
2023-09-10 17:27:06 +0200

    delete

--- src/components/SingleCommnent/SingleComment.jsx
+++ src/components/SingleCommnent/SingleComment.jsx
@@ -4,12 +4,13 @@ import { Button } from 'react-bootstrap'
 import {AiFillDelete} from 'react-icons/ai'
 
 const SingleComment = ({comment,asin,func}) => {
-    const url = "https://striveschool-api.herokuapp.com/api/comments/"
+    const url = "https://striveschool-api.herokuapp.com/api/comments/"+asin
     const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGVhNGJlMTUxNWY0MTAwMTQ2OTdhMmYiLCJpYXQiOjE2OTQxOTgzMjYsImV4cCI6MTY5NTQwNzkyNn0.1iZJTfNwWY-3XTgNKSc4CJr-k_Z6m-g_8efsrv0kOAA"
    
     const deleteComment = async () => {
+        console.log(url);
         try {
-            const response = await axios.delete(url+asin,{
+            const response = await axios.delete(url,{
                 headers: {
                     'Authorization': token
                 }})
