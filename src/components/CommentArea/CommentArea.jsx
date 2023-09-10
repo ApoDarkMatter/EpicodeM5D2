df96bf7cad9860a9f67024fbd50e7df95d52460a
ApoDarkMatter <ndrpln@gmail.com>
2023-09-10 10:37:07 +0200

    get comments

--- src/components/CommentArea/CommentArea.jsx
+++ src/components/CommentArea/CommentArea.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useEffect } from 'react'
 
-const CommentArea = ({comments}) => {
-  return (
-    <div>{comments}</div>
-  )
-}
+const CommentArea = ({asin}) => {
+  const url = "https://striveschool-api.herokuapp.com/api/comments/"
+  const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGVhNGJlMTUxNWY0MTAwMTQ2OTdhMmYiLCJpYXQiOjE2OTQxOTgzMjYsImV4cCI6MTY5NTQwNzkyNn0.1iZJTfNwWY-3XTgNKSc4CJr-k_Z6m-g_8efsrv0kOAA"
+  
+  const [comment,setComment] = useState([])
+
+  const getComments = async () => {
+    try {
+      const response = await axios.get(url+asin,{
+        headers: {
+            'Authorization': token
+        }})
+        if(response.data.comment!=null) {
+          setComment(response.data)
+        } else {
+          setComment("No Comments Here")
+        }
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getComments()
+  },[])
+
+    return (
+      <>
+        <li>{comment.comment} - {comment.rate}</li>
+      </>
+    )
+  }
 
 export default CommentArea
--- src/components/MyBookCard/MyBookCard.jsx
+++ src/components/MyBookCard/MyBookCard.jsx
@@ -1,33 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Card,Button } from 'react-bootstrap'
 import './MyBookCard.css'
 import CommentArea from '../CommentArea/CommentArea'
-import axios from 'axios'
 
 
 const MyBookCard = ({genre}) => {
-  const url = "https://striveschool-api.herokuapp.com/api/comments/"
-  const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGVhNGJlMTUxNWY0MTAwMTQ2OTdhMmYiLCJpYXQiOjE2OTQxOTgzMjYsImV4cCI6MTY5NTQwNzkyNn0.1iZJTfNwWY-3XTgNKSc4CJr-k_Z6m-g_8efsrv0kOAA"
-  
+ 
   const [selected,setSelected] = useState(false)
-  const [comment,setComment] = useState([])
-
-  const getComments = async () => {
-    try {
-      const response = await axios.get(url,{
-        headers: {
-            'Authorization': token
-        }})
-      setComment(response.data)
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
+  
+  const selFunction = () => {
+    setSelected(!selected)
   }
 
-  useEffect(() => {
-    getComments()
-  },[])
 
 /*   const postComments = async () => {
     try {
@@ -38,9 +22,7 @@ const MyBookCard = ({genre}) => {
     }
   }
  */
-  const selFunction = () => {
-    setSelected(!selected)
-  }
+
 
   if(selected) {
     return (     
@@ -53,7 +35,7 @@ const MyBookCard = ({genre}) => {
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
         </Card.Body>
-        <CommentArea comments={"diesci"}/>
+          <CommentArea asin={genre.asin}/>
       </Card>
     )
   } else {
